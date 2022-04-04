6b452ab8e9b65f03800519bf4b944ace529c2d74
Narendraprasath <92501792+Narendar0@users.noreply.github.com>
2022-04-04 19:10:31 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,22 @@ const Home = () => {
               <Typography variant='h3'>View All Products</Typography>
           </Button>
       </Box>
+<h2>Welcome To Book Store App</h2>
+      
+    <p>Books play a quintessential role in every student’s 
+      life by introducing them to a world of imagination,
+       providing knowledge of the outside world, improving
+        their reading, writing and speaking skills as well 
+        as boosting memory and intelligence. The importance 
+        of books in our life cannot be undermined for they 
+        not only help in broadening our horizons but also act as doorways
+         of connecting us with the world around us. 
+         They function as survival kits, they influence us
+          and leave an impact on us. Want to know the benefits
+           of books and the importance of reading? Curious about
+            how books impact our lives? Read this blog to know 
+            all about the importance of books in a student’s life,
+             essays on importance of reading books, quotes and more!</p>
   </div>
   )
 }
